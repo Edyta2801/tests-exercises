1b3596e9b27bb26a6437b1f7a450658ae81a708f
Edyta Szarowska <edyta.szarowska@interia.pl>
2018-12-10 12:23:46 +0100

    modify function to pass test with shipping

--- src/index.js
+++ src/index.js
@@ -1,13 +1,24 @@
 
-export const orderTotal = (arrayOfOrders) => (
-    arrayOfOrders.reduce(
-        (reduced, item) =>(
-        item.quantity!==undefined ?
-            reduced+(item.price*item.quantity)
-            :
-            reduced+ item.price
-            ), 0
+export const orderTotal = (arrayOfOrders) => {
+    const preTotal = arrayOfOrders.reduce(
+        (reduced, item) => (
+            item.quantity !== undefined ?
+                reduced + (item.price * item.quantity)
+                :
+                reduced + item.price
+        ),
+        0
     )
-)
 
+    const shipping = arrayOfOrders.find(item => item.shipping)
 
+    return (
+        shipping ?
+            preTotal >= (shipping.freeShipping + shipping.price) ?
+                preTotal - shipping.price
+                :
+                preTotal
+            :
+            preTotal
+    )
+}
